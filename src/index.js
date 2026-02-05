499fd6658c1625e59279637fd60b495003d0f65c
Karl MARQUES <karl@kmarques.dev>
2026-02-05 14:24:09 +0100

    # Write your commit message here
    Machine user: karl
    Machine system: Darwin
    GIT Username: Karl MARQUES
    Commit Date: 2026-02-05 14:24:09
    feat: jwt

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const process = require("process");
+
+if (!process.env.JWT_SECRET) {
+  console.error("JWT_SECRET environment variable is not set.");
+  process.exit(1);
+}
 
 const app = express();
 
@@ -9,6 +15,16 @@ app.get("/", (req, res) => {
 
 app.get("/health", (req, res) => {
   res.sendStatus(200);
+});
+
+app.get("/auth/login", (req, res) => {
+  const token = jwt.sign(
+    {
+      uid: Date.now(),
+    },
+    process.env.JWT_SECRET,
+  );
+  res.json({ token });
 });
 
 app.listen(process.env.PORT || 3000, () => {
